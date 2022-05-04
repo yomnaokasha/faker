66178546c810139048b80d462f9c4ba625f23854
yomnaokasha <youmna.okasha@gmail.com>
2022-05-04 14:49:50 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const faker = require("faker");
+
+const createUser = () => {
+  const newUser = {
+    id: faker.datatype.uuid(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+  return newUser;
+};
+
+const createCompany = () => {
+  const newCompany = {
+    id: faker.datatype.uuid(),
+    name: faker.company.companyName(),
+    address: {
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode(),
+      country: faker.address.country(),
+    },
+  };
+  return newCompany;
+};
+
+app.get("/api/users/new", (req, res) => {
+  const newUser = createUser();
+  res.json(newUser);
+});
+
+app.get("/api/companies/new", (req, res) => {
+  const newCompany = createCompany();
+  res.json(newCompany);
+});
+
+app.get("/api/user/company", (req, res) => {
+  const newUser = createUser();
+  const newCompany = createCompany();
+  res.json({ user: newUser, company: newCompany });
+});
+
+const server = app.listen(8000, () =>
+  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+);
